d1507b34e39cb814ed8495c5dd67be562cb3d6a2
João Vitor Oliveira Corrêa <correavitor4@hotmail.com>
2023-01-12 10:09:28 -0300

    Está buscando o macaco mais próximo

--- Part 1 - Displaying Data/MonkeyFinder/ViewModel/MonkeysViewModel.cs
+++ Part 1 - Displaying Data/MonkeyFinder/ViewModel/MonkeysViewModel.cs
@@ -7,12 +7,54 @@ public partial class MonkeysViewModel : BaseViewModel
 {
     MonkeyService monkeyService;
     IConnectivity connectivity;
+    IGeolocation geolocation;
     public ObservableCollection<Monkey> Monkeys { get; } = new();
-    public MonkeysViewModel(MonkeyService monkeyService, IConnectivity connectivity)
+    public MonkeysViewModel(MonkeyService monkeyService,
+        IConnectivity connectivity,
+        IGeolocation geolocation)
     {
         this.monkeyService = monkeyService;
         Title = "MonkeyFinder";
         this.connectivity = connectivity;
+        this.geolocation = geolocation;
+    }
+
+    [RelayCommand]
+    async Task GetClosestMonkeyAsync()
+    {
+        if (IsBusy || Monkeys.Count == 0)
+            return;
+
+        try
+        {
+            var location = await geolocation.GetLastKnownLocationAsync();
+            if (location is null || location.Timestamp.AddSeconds(30) < DateTime.UtcNow)
+            {
+                location = await geolocation.GetLocationAsync(
+                    new GeolocationRequest
+                    {
+                        DesiredAccuracy = GeolocationAccuracy.Medium,
+                        Timeout = TimeSpan.FromSeconds(30),
+                    });
+            }
+
+            if (location is null)
+                return;
+
+            var first = Monkeys.OrderBy(m => 
+                location.CalculateDistance(m.Latitude, m.Longitude, DistanceUnits.Miles)
+                ).FirstOrDefault();
+
+            if (first == null) return;
+
+            await Shell.Current.DisplayAlert("Closest Monkey", $"{first.Name} in {first.Location}", "Ok");
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine(ex);
+            await Shell.Current.DisplayAlert("Error!", "Unable to get closest monkey", "OK");
+            return;
+        }
     }
 
     [RelayCommand]
