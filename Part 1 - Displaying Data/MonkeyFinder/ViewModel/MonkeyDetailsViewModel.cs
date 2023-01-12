4dbcfeb718aee3d0885089c1e10e7f3fbf06b1b8
João Vitor Oliveira Corrêa <correavitor4@hotmail.com>
2023-01-12 10:18:00 -0300

    Está abrindo o mapa na localização do macaco

--- Part 1 - Displaying Data/MonkeyFinder/ViewModel/MonkeyDetailsViewModel.cs
+++ Part 1 - Displaying Data/MonkeyFinder/ViewModel/MonkeyDetailsViewModel.cs
@@ -5,17 +5,39 @@ namespace MonkeyFinder.ViewModel;
 [QueryProperty("Monkey","Monkey")]
 public partial class MonkeyDetailsViewModel : BaseViewModel
 {
-    public MonkeyDetailsViewModel() 
+    IMap Map;
+    public MonkeyDetailsViewModel(IMap map) 
     {
-    
+        Map = map;
     }
 
     [ObservableProperty]
     Monkey monkey;
 
+
     [RelayCommand]
     async Task GoBackAsync()
     {
         await Shell.Current.GoToAsync("..");
+    }
+
+    [RelayCommand]
+    async Task OpenMapAsync()
+    {
+        try
+        {
+            await Map.OpenAsync(monkey.Latitude, monkey.Longitude,
+                new MapLaunchOptions
+                {
+                    Name = monkey.Name,
+                    NavigationMode = NavigationMode.None,
+                });
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine(ex);
+            await Shell.Current.DisplayAlert("Error!", "Unable to open map", "OK");
+            return;
+        }
     }
 }
